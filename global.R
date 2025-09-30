f8230060b347997f7d4fc5e63ee249b68b256bb2
Simon <simon@elisya.org>
2025-09-30 09:26:48 +0100

    rename tax/taxon

--- global.R
+++ global.R
@@ -1,13 +1,16 @@
 require(sf)
 devtools::load_all("../BIADconnect/")
 source("R/tools.R")
-source("R/get.faunal.taxons.R")
+source("R/get.faunal.taxa.R")
+source("R/get.botanical.taxa.R")
 source("R/plots.R")
 
 
+conn <- BIADconnect::init.conn()
+
 area_of_interest  <- c( "France","Germany","Netherlands","Sweden", "Great Britain","Belgium","Luxembourg","Switzerland", "Ireland","Denmark","Czechia / Czech Republic","Poland", "Austria" ,"Andorra","Isle of Man", "Slovakia","Liechtenstein","Serbia","Croatia", "Hungary","Romania","Bosnia and Herzegovina","Bulgaria", "Latvia","North Macedonia","Ukraine","Slovenia", "Montenegro", "Guernsey","Russia","Estonia","Lithuania", "Albania","Kosovo","Moldova", "Norway","Finland", "Belarus")
 
-alltaxons <- getFaunalTaxons()
+alltaxons <- getFaunalTaxa()
 taxons_input  <-  alltaxons[ alltaxons$Country %in%  area_of_interest,]
 
 points_sf <- st_sfc(unique(st_geometry(alltaxons)))
@@ -20,4 +23,3 @@ if (file.exists("groupings/spatial/groups.RDS")) {
 } else {
     previous_polygon <- list()
 }
-conn <- BIADconnect::init.conn()

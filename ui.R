78d25b2a078af01d27ec13486de3261359250da9
Simon <simon@elisya.org>
2025-11-23 22:19:48 +0000

    add more information on the app

--- ui.R
+++ ui.R
@@ -8,15 +8,28 @@ library(geojsonsf)
 
 ui <- fluidPage(
   tags$div(
-    style = "background-color: #f8d7da; color: #721c24; padding: 10px; border-radius: 5px; margin-bottom: 20px;",
+    style = "background-color: #f8d7da; color: #721c24; padding: 10px; border-radius: 5px; margin-bottom: 20px; position:fixed;z-index:1000;width:100%;top:0;left:0",
     tags$h2("Warning: This is an experimental website for internal and testing use only.")
   ),
   leafletOutput("map", height='60vh'),
   verbatimTextOutput("groups"),
   actionButton("run_btn", "Run Analysis"),
   actionButton("clear_polygons_btn", "Clear Polygons"),
-  selectInput("file_selector_tx", "Select File to group taxon:", choices = list.files(path = "groupings/faunal_taxa/",pattern = "group_.*.csv",full.names = TRUE),selected="groupings/faunal_taxa/group_I.csv"),
-  selectInput("file_selector_per", "How to group periods", choices = list(Auto="auto","Period Name"="groupings/periods/periods.csv",selected="auto"),width="150px"),
+  # Create a fluidRow for the select input and tableOutput
+  fluidRow(
+    column(width = 6,
+           selectInput("file_selector_tx", "Select File to group taxon:", 
+                       choices = list.files(path = "groupings/faunal_taxa/", 
+                                            pattern = "group_.*.csv", 
+                                            full.names = TRUE),
+                       selected = "groupings/faunal_taxa/group_IV.csv"),
+             div(style = "font-size: 12px; margin-bottom: 10px;margin-left: 10px; margin-top: -10px;",  # Small text and close position
+           htmlOutput("taxon_table")  # Output for the table
+           )
+    ),
+  ),
+
+  selectInput("file_selector_per", "How to group periods", choices = list(Auto="auto","Period Name"="groupings/periods/periods.csv"),selected="auto",width="150px"),
   conditionalPanel(
     condition = "input.file_selector_per == 'auto'",
     fluidRow(
@@ -25,14 +38,17 @@ ui <- fluidPage(
       column(width = 2, sliderInput("duration", "Time slice duration:", min = 100, max = 1000, value = 500, step = 50, width = "100%"))
     )
   ),
-  fluidRow(
-    column(4, plotOutput("plot1", height = "300px")),
-    column(4, plotOutput("plot2", height = "300px"))
-    ),
-  fluidRow(
-    column(width=6, plotOutput("plot3", height = "500px",width="100%"))
+HTML("<p><b>Figure 1:</b> <br/> Raw data. Left Total number of NISP for each phase in each selected region, right, percentage of each group in each phase for each area </p>"),
+fluidRow(
+  column(4, plotOutput("plot1", height = "300px")),
+  column(4, plotOutput("plot2", height = "300px"))
   ),
-  fluidRow(
-    column(width=6, plotOutput("plot4", height = "500px",width="100%"))
+HTML("<p><b>Figure 2:</b> <br/> Result of the Correspondance Analysis, with arrow representing the order of the phases</p>"),
+fluidRow(
+  column(width=6, plotOutput("plot3", height = "500px",width="100%"))
+),
+HTML("<p><b>Figure 3:</b> <br/> Split of the two dimension of the CA.</p>"),
+fluidRow(
+  column(width=6, plotOutput("plot4", height = "500px",width="100%"))
   )
 )

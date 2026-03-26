483ee35142d46f332fb26b177709da3584dfdbf3
Simon <simon@elisya.org>
2026-03-26 16:27:07 +0000

    make the taxa explorer global again

--- global.R
+++ global.R
@@ -18,20 +18,8 @@ source("R/ui_components.R")
 
 conn <- BIADconnect::init.conn()
 
-area_of_interest <- c(
-  "France", "Germany", "Netherlands", "Sweden", "Great Britain", "Belgium",
-  "Luxembourg", "Switzerland", "Ireland", "Denmark", "Czechia / Czech Republic",
-  "Poland", "Austria", "Andorra", "Isle of Man", "Slovakia", "Liechtenstein",
-  "Serbia", "Croatia", "Hungary", "Romania", "Bosnia and Herzegovina", "Bulgaria",
-  "Latvia", "North Macedonia", "Ukraine", "Slovenia", "Montenegro", "Guernsey",
-  "Russia", "Estonia", "Lithuania", "Albania", "Kosovo", "Moldova", "Norway",
-  "Finland", "Belarus"
-)
-
 allFTaxa <- getFaunalTaxa(conn = conn)
 allBTaxa <- getABotTaxa(conn = conn)
-allFTaxa <- allFTaxa[allFTaxa$Country %in% area_of_interest, ]
-allBTaxa <- allBTaxa[allBTaxa$Country %in% area_of_interest, ]
 
 faunalSites <- unique_site_points(allFTaxa)
 botanicalSites <- unique_site_points(allBTaxa)
